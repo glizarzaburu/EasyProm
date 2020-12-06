f3168c959ac6fc858ec57fd480e1fd0565f2c7d2
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2020-12-06 01:17:00 -0500

    Clases para manejar el adapter de la lista de productos hechas
    Con unos cuantos placeholders para el tema de la data

--- app/src/main/java/com/easyprom/lista_productos/ListaProductosAdapter.kt
+++ app/src/main/java/com/easyprom/lista_productos/ListaProductosAdapter.kt
@@ -1,5 +1,6 @@
 package com.easyprom.lista_productos
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
@@ -7,23 +8,32 @@ import androidx.recyclerview.widget.RecyclerView
 import com.easyprom.databinding.ItemProductoBinding
 import com.easyprom.entidades.Producto
 
-class ListaProductosAdapter :
+class ListaProductosAdapter(private val clickListener: ProductoListener) :
     ListAdapter<Producto, ListaProductosAdapter.ListaProductosViewHolder>(ListaProductosDiffCallback()) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListaProductosViewHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ListaProductosViewHolder =
+        ListaProductosViewHolder.from(parent)
 
     override fun onBindViewHolder(holder: ListaProductosViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val producto = getItem(position)
+        holder.bind(producto, clickListener)
     }
 
     class ListaProductosViewHolder(private val binding: ItemProductoBinding) :
         RecyclerView.ViewHolder(binding.root) {
 
-        fun bind(producto: Producto) {
+        fun bind(producto: Producto, clickListener: ProductoListener) {
             // eventualmente acá se pasará la info del producto para que aparezca en el layout
+            // y el evento de click
             binding.executePendingBindings()
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): ListaProductosViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = ItemProductoBinding.inflate(layoutInflater, parent, false)
+                return ListaProductosViewHolder(binding)
+            }
         }
     }
 
@@ -38,4 +48,8 @@ class ListaProductosDiffCallback : DiffUtil.ItemCallback<Producto>() {
 
     override fun areContentsTheSame(oldItem: Producto, newItem: Producto): Boolean =
         oldItem == newItem
+}
+
+class ProductoListener(val clickListener: (producto: Producto) -> Unit) {
+    fun onClick(producto: Producto) = clickListener(producto)
 }
