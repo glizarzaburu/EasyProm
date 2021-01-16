4582e785ddf49a9cfca5ccfcfc5615c3e7ce3a1e
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2021-01-15 23:43:45 -0500

    Agregada navegación al detalle del plato y se muestra la información del mismo

--- app/src/main/java/com/easyprom/detalle_plato/DetallePlatoFragment.kt
+++ app/src/main/java/com/easyprom/detalle_plato/DetallePlatoFragment.kt
@@ -1,6 +1,29 @@
 package com.easyprom.detalle_plato
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
+import com.easyprom.databinding.FragmentDetallePlatoBinding
 
 class DetallePlatoFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetallePlatoBinding
+    private val args: DetallePlatoFragmentArgs by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        binding = FragmentDetallePlatoBinding.inflate(inflater, container, false)
+        binding.lifecycleOwner = this
+
+        binding.plato = args.plato
+
+        return binding.root
+    }
 }
--- app/src/main/java/com/easyprom/lista_platos/ListaProductosFragment.kt
+++ app/src/main/java/com/easyprom/lista_platos/ListaProductosFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import com.easyprom.databinding.FragmentListaplatosBinding
 
 class ListaProductosFragment : Fragment() {
@@ -24,8 +26,12 @@ class ListaProductosFragment : Fragment() {
         binding.viewModel = viewModel
 
         val adapter = ListaProductosAdapter(ProductoListener { plato ->
-            // acá manejaremos el evento de click. Por ahora un mensaje en consola
-            println("El plato es $plato")
+            findNavController()
+                .navigate(
+                    ListaProductosFragmentDirections.actionListaProductosFragmentToDetallePlatoFragment(
+                        plato
+                    )
+                )
         })
         binding.productosLista.adapter = adapter
 
