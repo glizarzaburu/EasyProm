8798cd5e1669b433221b90e99bc30ca8a53036b7
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2021-02-05 15:21:50 -0500

    Agregada navegacion a 'agregar platos' fragment

---
+++ app/src/main/java/com/akipa/ui/mantenimiento_platos/agregar_plato/AgregarPlatoFragment.kt
@@ -0,0 +1,7 @@
+package com.akipa.ui.mantenimiento_platos.agregar_plato
+
+import androidx.fragment.app.Fragment
+import com.akipa.R
+
+class AgregarPlatoFragment : Fragment(R.layout.fragment_agregar_plato) {
+}
