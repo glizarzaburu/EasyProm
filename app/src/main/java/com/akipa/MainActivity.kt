bc2428aeddfac3928b13cfbfb1a7bbbb4727f389
Juan Carlos Castillo Aycachi <carlosjordi@hotmail.com>
2021-02-04 20:01:32 -0500

    Agregado DrawerLayout

--- app/src/main/java/com/akipa/MainActivity.kt
+++ app/src/main/java/com/akipa/MainActivity.kt
@@ -2,22 +2,32 @@ package com.akipa
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
+import com.akipa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setTheme(R.style.Theme_EasyProm)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        drawerLayout = binding.drawerLayout
 
         // Para manejar la navegación con el 'up button'
         val navController = findNavController(R.id.my_nav_host_fragment)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navigationView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return findNavController(R.id.my_nav_host_fragment).navigateUp()
+        val navControler = findNavController(R.id.my_nav_host_fragment)
+        return NavigationUI.navigateUp(navControler, drawerLayout)
     }
 }
